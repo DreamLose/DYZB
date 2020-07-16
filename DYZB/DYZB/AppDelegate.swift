ecd1c452220990c3050c59140ef37abbcc453271
DreamLose <1083531814@qq.com>
2020-07-16 16:18:00 +0800

    初始化项目

---
+++ DYZB/DYZB/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  DYZB
+//
+//  Created by 2020 on 2020/7/16.
+//  Copyright © 2020 2020. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
